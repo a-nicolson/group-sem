e96fc7f0f69dc1a42c75dc0cdf83ccdcd86185e7
Alice Nicolson <407233265@live.napier.ac.uk>
2025-10-14 11:46:11 +0100

    Removed MongoDB dependencies

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main(String[] args)
     {
-       System.out.println("SQL connection to go here");
+       System.out.println("SQL connection to go here.");
     }
 }
