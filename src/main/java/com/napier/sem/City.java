9e2748e3413b29fa80e9b57e438c81da2d7d5252
Alice Nicolson <407233265@live.napier.ac.uk>
2025-11-11 02:33:34 +0000

    Added functionality to produce city, country reports

---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,15 @@
+package com.napier.sem;
+
+/**
+ *  Class representing a city object.
+ *  A city has a name and population and is
+ *  part of a district, country, region and continent
+ */
+public class City {
+
+    public String cityName;
+    public int cityID;
+    public String countryCode;
+    public String district;
+    public int population;
+}
---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,15 @@
+package com.napier.sem;
+
+/**
+ *  A class representing a Country object.
+ *  A Country has name, population and a list of cities.
+ */
+public class Country {
+    public String countryName;
+    public String countryCode;
+    public String continent;
+    public String region;
+    public int population;
+    public int capitalID;
+    public int surfaceArea;
+}
